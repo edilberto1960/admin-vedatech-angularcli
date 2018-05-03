eeecc12789b892154807599da1049252aee130e5
edilberto1960 <redilberto@gmail.com>
2018-05-03 18:38:08 -0500

    first commit

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styleUrls: ['./progress.component.css']
+  styleUrls: []
 })
 export class ProgressComponent implements OnInit {
 
